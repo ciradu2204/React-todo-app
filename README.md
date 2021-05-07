9094cf37df469528832776fcedae86e5f34eeabd
ciradu2204 <c.iradukund@alustudent.con>
2021-05-07 10:45:31 +0200

    edited the description

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # A To-do app developed using React.js
 
 In this app, you can add, delete, submit and edit items. To edit items,
-simply double click on it. Once you are done, press the enter key 
+simply click the edit icon. Once you are done, press the enter key 
 to resubmit. This app will persist your data in the browser local 
 storage. So whether you reload, close your app or reopened it,
 you still have access to your to-dos items.
--- src/pages/SinglePage.js
+++ src/pages/SinglePage.js
@@ -8,7 +8,7 @@ const SinglePage = () => {
             slug: "about-app",
             title: "About the App",
             description:
-                "In this app, you can add, delete, submit and edit items. To edit items, simply double click on it. Once you are done, press the enter key to resubmit. This app will persist your data in the browser local storage. So whether you reload, close your app or reopened it, you still have access to your to-dos items.",
+                "In this app, you can add, delete, submit and edit items.  To edit items, simply click the edit icon. Once you are done, press the enter key to resubmit. This app will persist your data in the browser local storage. So whether you reload, close your app or reopened it, you still have access to your to-dos items.",
         },
         {
             slug: "about-author",
